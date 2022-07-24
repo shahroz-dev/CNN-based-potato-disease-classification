4b3043d640651c628b5661e5fa8bdb65ee815a94
Muhammad Shahroz <shahrozarif28@gmail.com>
2022-07-24 23:08:37 +0500

    first commit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import numpy as np
 from io import BytesIO
@@ -8,19 +7,7 @@ import tensorflow as tf
 
 app = FastAPI()
 
-origins = [
-    "http://localhost",
-    "http://localhost:3000",
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-MODEL = tf.keras.models.load_model("model/c")
+MODEL = tf.keras.models.load_model("model/1")
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
 
@@ -30,8 +17,9 @@ async def ping():
     return "Hello, I am alive"
 
 
-def read_file_as_image(data) -> np.ndarray:
+def read_file_as_image(data):
     image = np.array(Image.open(BytesIO(data)))
+    image = image/255
     return image
 
 
@@ -54,3 +42,6 @@ async def predict(
 
 if __name__ == "__main__":
     uvicorn.run(app, host='localhost', port=8000)
+
+# Docker command
+# docker run -t --rm -p 8501:8501 -v C:/Code/potato-disease:/potato-disease tensorflow/serving --rest_api_port=8501 --model_config_file=/potato-disease/models.config
